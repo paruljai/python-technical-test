9832b980ad5ab6776ad7d8ae6de29459aa506396
Ben Rometsch <ben.rometsch@flagsmith.com>
2021-05-24 16:06:02 +0100

    Added REST API question and removed existing one. (#1)
    * Added REST API question and removed existing one.
    
    * updated guidance
    
    * Updated email address

--- src/question3/search.py
+++
@@ -1,9 +0,0 @@
-class Search:
-    """Base class for a generic Search"""
-
-    def search(self, query: str) -> list:
-        """ Perform Search for a query.
-
-        :param query a single search query represented as a String.
-        :return A List of search results from this search"""
-        pass
